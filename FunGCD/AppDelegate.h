d9fc7c001fef49010caf869d80377bd2591e5475
compass <287846224@qq.com>
2016-05-28 15:02:42 +0800

    Initial Commit

---
+++ FunGCD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunGCD
+//
+//  Created by JiaLi on 16/5/28.
+//  Copyright © 2016年 JiaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunGCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunGCD
+//
+//  Created by JiaLi on 16/5/28.
+//  Copyright © 2016年 JiaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
